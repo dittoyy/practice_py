0310c1aa8b87bcb66756625ce7409330e0f4a2a1
ditto <Ditto.He@r-pac.com.cn>
2017-08-14 15:31:08 +0800

    this is about pagefactory test

---
+++ pf1.py
@@ -0,0 +1,22 @@
+from pageobject_support import callable_find_by as find_by
+from selenium import webdriver
+
+class BaiduSearchPage(object):
+
+    def __init__(self, driver):
+        self._driver = driver
+
+    search_box = find_by(id_="kw")
+    search_button = find_by(id_='su')
+
+    def search(self, keywords):
+        self.search_box().clear()
+        self.search_box().send_keys(keywords)
+        self.search_button().click()
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get("https://www.baidu.com")
+    BaiduSearchPage(driver).search("selenium")
+    driver.close()
